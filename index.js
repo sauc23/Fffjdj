5dd000c6478c9ea1190b8115349f4dd8cd063359
bruh <89742348+sauc23@users.noreply.github.com>
2024-12-12 12:52:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
 import http from 'node:http';
 import { createBareServer } from '@tomphttp/bare-server-node';
-import { HttpsProxyAgent } from 'https-proxy-agent'; // Ensure correct import
-
-const httpProxyAgent = new HttpsProxyAgent('https://e3z4acki-gv85bq9:ksc8d3rmv7@us-east-111.totallyacdn.com');
 
 const httpServer = http.createServer();
-
-const bareServer = createBareServer('/', {
-	httpAgent: httpProxyAgent,
-	httpsAgent: httpProxyAgent,
-});
+const bareServer = createBareServer('/');
 
 httpServer.on('request', (req, res) => {
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeRequest(req, res);
-	} else {
-		res.writeHead(400);
-		res.end('Not found.');
-	}
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeRequest(req, res);
+    } else {
+        res.writeHead(400);
+        res.end('Not found.');
+    }
 });
 
 httpServer.on('upgrade', (req, socket, head) => {
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeUpgrade(req, socket, head);
-	} else {
-		socket.end();
-	}
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
 });
 
 httpServer.on('listening', () => {
-	console.log('HTTP server listening');
+    console.log('HTTP server listening');
 });
 
-httpServer.listen({
-	port: 8080,
-});
+httpServer.listen({ port: 8080 });
